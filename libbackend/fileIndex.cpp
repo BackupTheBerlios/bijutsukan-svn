95a2bcb3fe9a6440eb361606649447e1ebcbecc1
linuxcommando <linuxcommando@093ea02d-1409-0410-a765-b2953bfdc763>
2006-01-30 19:07:47 +0000

    Started to comment fileIndex

--- libbackend/fileIndex.cpp
+++ libbackend/fileIndex.cpp
@@ -1,32 +1,54 @@
 #include "fileIndex.h"
-
+/**
+ * fileIndex class constructor
+**/
 fileIndex::fileIndex()
 {
 	//TODO: do something useful here :)
 }
-
+/**
+ * Returns a wxArrayString pointee to pathIndex of our fileIndex class.
+**/
 wxArrayString* fileIndex::returnPathIndex()
 {
   return &pathIndex;
 }
 
+/**
+ * Looks for <wxString value> inside pathIndex.
+ * <bool caseSensitive> enables/disables case sensitive search.
+ * TODO: what happens if filepath is empty?
+**/
 int fileIndex::findValue(wxString value, bool caseSensitive)
 {
   return pathIndex.Index(value, caseSensitive);
 }
 
+/**
+ * linuxcommando: i think this should be removed completely because the function above offers more control.
+ * The behavior of this function concerning case sensitivity might be unexpected due to wxWidgets API changes 
+**/
+/*
 int fileIndex::findValue(wxString value)
 {
 
   return pathIndex.Index(value);
 }
+*/
 
-
+/**
+ * Adds filename specified by <wxString filepath> to pathIndex.
+ * TODO: what happens if filepath is empty?
+**/
 void fileIndex::addFile(wxString filePath)
 {
   pathIndex.Add( filePath.wc_str() ); // maybe mb_str instead of wc_str
 }
 
+/**
+ * Deletes filename specified by <wxString filepath> from pathIndex.
+ * TODO: what happens if filepath is empty?
+**/
 void fileIndex::delFile(wxString filePath)
 {
   pathIndex.Remove( filePath.wc_str() );
--- libbackend/fileIndex.h
+++ libbackend/fileIndex.h
@@ -13,13 +13,31 @@ class fileIndex
 {
 
  public:
+
   fileIndex();
+  	/**
+	 * fileIndex class constructor
+	**/
   wxArrayString* returnPathIndex();
+  	/**
+	 * Returns a wxArrayString pointee to pathIndex of our fileIndex class.
+	**/
   int findValue(wxString value, bool caseSensitive);
-  int findValue(wxString value);
+  	/**
+	 * Looks for <wxString value> inside pathIndex.
+  	 * <bool caseSensitive> enables/disables case sensitive search.
+	**/
+  //int findValue(wxString value);
+  	// linuxcommando: this function should be removed, see source file for details
 
   void addFile(wxString filePath);
+	/**
+	 * Adds filename specified by <wxString filepath> to pathIndex.
+	**/
   void delFile(wxString filePath);
+  	/**
+  	 * Deletes filename specified by <wxString filepath> from pathIndex.
+  	**/
   imageObject* makeImageObject(wxString filePath);
 
  private:

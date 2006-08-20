423cf2e604828eaf72c7183bb710d14a42e6fdd5
nebuk <nebuk@093ea02d-1409-0410-a765-b2953bfdc763>
2006-08-20 20:49:41 +0000

    a bit less ugly - seperators need less space now

--- trunk/frontend/showFileDialog.py
+++ trunk/frontend/showFileDialog.py
@@ -8,6 +8,7 @@ gladefile = "glade/bijutsukan.glade"
 class showFileDialog:
   def __init__(self, bild):
     self.Bild = bild
+    print self.Bild.Attributes
     self.wTree = gtk.glade.XML(gladefile, "showFileDialog")
     self.dlg = self.wTree.get_widget("showFileDialog")
     self.getWidgets()
@@ -21,33 +22,56 @@ class showFileDialog:
     self.image.set_from_pixbuf(pbuf)
 
     self.name.set_text("Name: %s"%self.Bild.Attributes["name"])
-    self.filename.set_text("Filename: %s"%self.Bild.Path)
+    #self.filename.set_text("Filename: %s"%self.Bild.Path)
     self.category.set_text("Category: %s"%self.Bild.Attributes["category"])
 
     self.tagsColumn = gtk.TreeViewColumn("", gtk.CellRendererText(), text=0)
     self.tags.append_column(self.tagsColumn)
     self.tagsModel = gtk.ListStore(str)
     self.tags.set_model(self.tagsModel)
+    for item in self.Bild.Attributes["tags"]:
+      self.tagsModel.append(item)
 
     self.personsColumn = gtk.TreeViewColumn("", gtk.CellRendererText(), text=0)
     self.persons.append_column(self.personsColumn)
     self.personsModel = gtk.ListStore(str)
     self.persons.set_model(self.personsModel)
+    for item in self.Bild.Attributes["persons"]:
+      self.personsModel.append(item)
+    
 
     self.objectsColumn = gtk.TreeViewColumn("", gtk.CellRendererText(), text=0)
-    self.objects.append_column(self.personsColumn)
+    self.objects.append_column(self.objectsColumn)
     self.objectsModel = gtk.ListStore(str)
-    self.objects.set_model(self.personsModel)
+    self.objects.set_model(self.objectsModel)
+    for item in self.Bild.Attributes["objects"]:
+      self.objectsModel.append(item)
 
     self.placesColumn = gtk.TreeViewColumn("", gtk.CellRendererText(), text=0)
     self.places.append_column(self.placesColumn)
     self.placesModel = gtk.ListStore(str)
     self.places.set_model(self.placesModel)
+    for item in self.Bild.Attributes["places"]:
+      self.placesModel.append(item)
 
     self.positionsColumn = gtk.TreeViewColumn("", gtk.CellRendererText(), text=0)
-    self.positions.append_column(self.placesColumn)
+    self.positions.append_column(self.positionsColumn)
     self.positionsModel = gtk.ListStore(str)
-    self.positions.set_model(self.placesModel)
+    self.positions.set_model(self.positionsModel)
+    for item in self.Bild.Attributes["positions"]:
+      self.positionsModel.append(item)
+
+    self.exifColumns = [
+      gtk.TreeViewColumn("Exif Name", gtk.CellRendererText(), text=0),
+      gtk.TreeViewColumn("Exif Value", gtk.CellRendererText(), text=1)
+      ]
+    self.exif.append_column(self.exifColumns[0])
+    self.exif.append_column(self.exifColumns[1])
+    self.exifModel = gtk.ListStore(str,str)
+    self.exif.set_model(self.exifModel)
+    for k,v in self.Bild.Attributes["exif"].iteritems():
+      self.exifModel.append([k,v])
+
 
     self.descriptionBuffer = gtk.TextBuffer()
     self.descriptionBuffer.set_text(self.Bild.Attributes["description"])
@@ -58,7 +82,7 @@ class showFileDialog:
 
   def getWidgets(self):
     self.image = self.wTree.get_widget("image")
-    self.filename = self.wTree.get_widget("filename")
+    #self.filename = self.wTree.get_widget("filename")
     self.name = self.wTree.get_widget("name")
     self.category = self.wTree.get_widget("category")
     self.tags = self.wTree.get_widget("tags")
@@ -66,6 +90,8 @@ class showFileDialog:
     self.objects = self.wTree.get_widget("objects")
     self.places = self.wTree.get_widget("places")
     self.positions = self.wTree.get_widget("positions")
-    self.description = self.wTree.get_widget("description")    
+    self.description = self.wTree.get_widget("description")
+    self.exif = self.wTree.get_widget("exif")
+    
 
 

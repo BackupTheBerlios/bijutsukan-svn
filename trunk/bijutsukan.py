02fd4315d83c1a8194c78dcf11e7d4201e749470
nebuk <nebuk@093ea02d-1409-0410-a765-b2953bfdc763>
2006-08-20 00:35:34 +0000

    mainWindow now creating rudimentary ui and showing some data

--- trunk/bijutsukan.py
+++ trunk/bijutsukan.py
@@ -12,10 +12,19 @@ class Bijutsukan:
 	def __init__(self):
 		self.wTree = gtk.glade.XML(gladefile, "mainWindow") 
 		self.wTree.signal_autoconnect({
+			"onMainQuit":gtk.main_quit,
 			"on_mainWindow_destroy" : gtk.main_quit,
-			"onAddImage" : self.onAddImage
+			"onAddImage" : self.onAddImage,
+			"onMainRowClick":self.onMainRowClick
 			})
-		self.pBackend = backend.pBackend(os.path.abspath(os.path.expanduser("~/.bijutsuStorage")))
+		self.Backend = backend.pBackend(os.path.abspath(os.path.expanduser("~/.bijutsuStorage")))
+		self.getWidgets()
+		self.createGUI()
+		self.getBilds()
+
+	def onMainRowClick(self, widget, column, view):
+		print "showing dialog for"
+		print self.bilds[column[0]].Attributes["name"]
 		
         def onAddImage(self, widget):
 		fdlg = frontend.singleFileChooser()
@@ -24,9 +33,36 @@ class Bijutsukan:
 			cdlg = frontend.singleInputDialog(fname)
 			bild, cresult = cdlg.run()
 			if cresult == -5:
-				self.pBackend.storeBild(bild)
-				
-	
+				self.Backend.storeBild(bild)
+
+	def createGUI(self):
+		self.mainViewColumns = [
+			gtk.TreeViewColumn("Thumbnail", gtk.CellRendererPixbuf(), pixbuf=0),
+			gtk.TreeViewColumn("Name", gtk.CellRendererText(), text=1),
+			gtk.TreeViewColumn("Category", gtk.CellRendererText(), text=2)
+			]
+		for column in self.mainViewColumns:
+			self.mainView.append_column(column)
+		self.mainViewModel = gtk.ListStore(gtk.gdk.Pixbuf, str, str)
+		self.mainView.set_model(self.mainViewModel)
+		self.getBilds()
+
+	def getWidgets(self):
+		self.mainView = self.wTree.get_widget("mainView")
+
+	def getBilds(self):
+		self.mainViewModel.clear()
+		self.bilds=[]
+		for k, item in self.Backend.listBilder({}).iteritems():
+			self.bilds.append(item)
+			tmp = gtk.gdk.PixbufLoader()
+			tmp.write(item.getBildBin())
+			pbuf = tmp.get_pixbuf().scale_simple(100, 100 ,gtk.gdk.INTERP_NEAREST)
+			self.mainViewModel.append([
+				pbuf,
+				item.Attributes["name"],
+				item.Attributes["category"]
+				])	
 
         
             
